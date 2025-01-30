6b79f58bfe4d8edf49c4a7dec320e3b39261ffb9
hossein <hossein@gmail.com>
2025-01-30 20:24:45 +0330

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const container = document.querySelector('#container');
+const changeBtn = document.querySelector('#changeBtn');
+const text = document.querySelector('#text');
+const input = document.querySelector('#input');
+
+let randomNumber = Math.floor(Math.random() * 100);
+
+
+changeBtn.addEventListener('click', () => {
+    console.log(randomNumber);
+    let inputVal = +input.value
+    if (inputVal === randomNumber) {
+        correctHandeler()
+    } else if (inputVal > randomNumber) {
+        biggerHandeler()
+    } else if (inputVal < randomNumber) {
+        smallerHandeler()
+    } else {
+        alert('Please choose a number:')
+    }
+    input.value = ''
+})
+function correctHandeler() {
+
+    text.textContent = 'Great! you choose a right number';
+    input.remove()
+    container.style.backgroundColor = '#46f046'
+    container.style.color = '#fff'
+    changeBtn.remove()
+
+}
+
+function biggerHandeler() {
+    text.textContent = 'your number is bigger';
+
+}
+function smallerHandeler() {
+    text.textContent = 'your number is smaller';
+
+}
+
+
